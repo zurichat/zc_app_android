56c3dd7448ee8a2484a85fcf9080054b87719b23
Peculiar Umeh <35475543+peculiaruc@users.noreply.github.com>
2021-08-28 17:04:42 +0100

    #10 Login XML

---
+++ app/src/main/java/com/tolstoy/zurichat/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.tolstoy.zurichat.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tolstoy.zurichat.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
