503b31c377480704687633ff24715492f775b7f6
Success0452 <olatundesuccess54@gmail.com>
2021-08-31 01:00:41 +0100

    Implemented Side Bar Navigation issue #21 updated loginActivity to link into MainActivity Implemented Toolbar and TabLayout in MainActivity

---
+++ app/src/main/java/com/tolstoy/zurichat/navigation_bar_component/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.tolstoy.zurichat.navigation_bar_component
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tolstoy.zurichat.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
--- app/src/main/java/com/tolstoy/zurichat/ui/login/LoginActivity.kt
+++ app/src/main/java/com/tolstoy/zurichat/ui/login/LoginActivity.kt
@@ -1,12 +1,22 @@
 package com.tolstoy.zurichat.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.tolstoy.zurichat.R
+import com.tolstoy.zurichat.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnVerifyEmail.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
