34b223c38b63b4f3d203ab3d43e2bb5caf61c642
Auwalu Hamza <45801980+afhamu@users.noreply.github.com>
2021-08-31 10:00:39 +0100

    feat: implemented onboarding screens

--- app/src/main/java/com/tolstoy/zurichat/ui/onboarding/SlideOneFragment.java
+++ app/src/main/java/com/tolstoy/zurichat/ui/onboarding/SlideOneFragment.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
@@ -15,18 +17,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.tbuonomo.viewpagerdotsindicator.DotsIndicator;
 import com.tolstoy.zurichat.R;
 import com.tolstoy.zurichat.ui.login.LoginActivity;
 
 
-public class SlideThreeFragment extends Fragment {
-
+public class SlideOneFragment extends Fragment {
+    private ViewPager viewPager;
     private String show_intro = "Show Intro";
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
-    private ViewPager viewPager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,25 +38,33 @@ public class SlideThreeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_slide_three, container, false);
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_slide_one, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        viewPager = view.getRootView().findViewById(R.id.pager);
         sharedPreferences = getActivity().getSharedPreferences("onboarding", Context.MODE_PRIVATE);
         editor = sharedPreferences.edit();
-
-        Button btnNext = view.findViewById(R.id.btn_next);
+        viewPager = view.getRootView().findViewById(R.id.pager);
         TextView skip = view.findViewById(R.id.skip);
 
-        btnNext.setOnClickListener(v -> {
+        if (!sharedPreferences.getBoolean(show_intro, true)){
+            startActivity(new Intent(getActivity(), LoginActivity.class));
+        }
+
+        skip.setOnClickListener((View v) -> {
             startActivity(new Intent(getActivity(), LoginActivity.class));
             editor.putBoolean(show_intro, false);
             editor.apply();
         });
-        skip.setOnClickListener(v -> startActivity(new Intent(getActivity(), LoginActivity.class)));
+
+        Button btnNext = view.findViewById(R.id.btn_next);
+        btnNext.setOnClickListener(v -> {
+            viewPager.setCurrentItem(viewPager.getCurrentItem()+1);
+        });
+
     }
 }
--- app/src/main/java/com/tolstoy/zurichat/ui/onboarding/SlideTwoFragment.java
+++ app/src/main/java/com/tolstoy/zurichat/ui/onboarding/SlideTwoFragment.java
@@ -1,13 +1,13 @@
 package com.tolstoy.zurichat.ui.onboarding;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
@@ -21,11 +21,8 @@ import com.tolstoy.zurichat.R;
 import com.tolstoy.zurichat.ui.login.LoginActivity;
 
 
-public class SlideThreeFragment extends Fragment {
+public class SlideTwoFragment extends Fragment {
 
-    private String show_intro = "Show Intro";
-    private SharedPreferences sharedPreferences;
-    private SharedPreferences.Editor editor;
     private ViewPager viewPager;
 
     @Override
@@ -36,7 +33,7 @@ public class SlideThreeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_slide_three, container, false);
+        return inflater.inflate(R.layout.fragment_slide_two, container, false);
     }
 
     @Override
@@ -44,17 +41,13 @@ public class SlideThreeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         viewPager = view.getRootView().findViewById(R.id.pager);
-        sharedPreferences = getActivity().getSharedPreferences("onboarding", Context.MODE_PRIVATE);
-        editor = sharedPreferences.edit();
+        TextView skip = view.findViewById(R.id.skip);
+        skip.setOnClickListener(v -> startActivity(new Intent(getActivity(), LoginActivity.class)));
 
         Button btnNext = view.findViewById(R.id.btn_next);
-        TextView skip = view.findViewById(R.id.skip);
-
         btnNext.setOnClickListener(v -> {
-            startActivity(new Intent(getActivity(), LoginActivity.class));
-            editor.putBoolean(show_intro, false);
-            editor.apply();
+            viewPager.setCurrentItem(viewPager.getCurrentItem()+1);
         });
-        skip.setOnClickListener(v -> startActivity(new Intent(getActivity(), LoginActivity.class)));
+
     }
 }
