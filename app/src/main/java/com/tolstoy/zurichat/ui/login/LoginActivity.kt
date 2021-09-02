0ad291d340a12b08224244687680796a586618d9
EricoMartin <martinirex@yahoo.co.uk>
2021-09-02 12:48:19 +0100

    bug: fix tool bar not set, fix appbar not covering the full width of screen, add search icon to display on toolbar

--- app/src/main/java/com/tolstoy/zurichat/ui/login/LoginActivity.kt
+++ app/src/main/java/com/tolstoy/zurichat/ui/login/LoginActivity.kt
@@ -1,12 +1,21 @@
 package com.tolstoy.zurichat.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.tolstoy.zurichat.R
+import com.tolstoy.zurichat.ui.activities.HomePageActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val button: Button = findViewById(R.id.btn_verify_email)
+
+        button.setOnClickListener {
+            val int = Intent(this, HomePageActivity::class.java)
+            startActivity(int)
+        }
     }
 }
