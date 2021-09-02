6cfcb29394029fb7d827f7a67db37115e94186b3
Abdulhamid Oyedele Oyempemi <oyempemia@gmail.com>
2021-09-02 17:30:12 +0100

    feat: manage storage settings screens.

--- app/src/main/java/com/tolstoy/zurichat/ui/login/LoginActivity.kt
+++ app/src/main/java/com/tolstoy/zurichat/ui/login/LoginActivity.kt
@@ -1,12 +1,17 @@
 package com.tolstoy.zurichat.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.tolstoy.zurichat.R
+import com.tolstoy.zurichat.ui.activities.MainActivity
+import com.tolstoy.zurichat.ui.settings.SettingsActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
     }
 }
