acb2dbae3077968a7215d0e68f98747734b332f2
Nwevo Peace Chinedu <nwevopeacechinedu@gmail.com>
2021-09-01 23:39:40 -0700

    added the email image to verify screen and created the verify otp screen

---
+++ app/src/main/java/com/tolstoy/zurichat/ui/activities/EnterCodeActivity.kt
@@ -0,0 +1,12 @@
+package com.tolstoy.zurichat.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tolstoy.zurichat.R
+
+class EnterCodeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_enter_code)
+    }
+}
