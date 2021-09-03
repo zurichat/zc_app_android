31bc8803018534f466b9aa293720a754720dd775
Ab3id <abeidtwo@gmail.com>
2021-09-03 10:44:46 +0300

    feat: implemented user ProfileActivity.kt

---
+++ app/src/main/java/com/tolstoy/zurichat/ui/activities/ProfileActivity.kt
@@ -0,0 +1,15 @@
+package com.tolstoy.zurichat.ui.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tolstoy.zurichat.R
+
+class ProfileActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+
+    }
+}
