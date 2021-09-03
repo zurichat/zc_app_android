5920210f295d931d3c27b13dffd33a30d00f966e
Nwevo Peace Chinedu <nwevopeacechinedu@gmail.com>
2021-09-03 14:35:03 -0800

    Email verification screen

--- app/src/main/java/com/tolstoy/zurichat/ui/login/LoginActivity.kt
+++ app/src/main/java/com/tolstoy/zurichat/ui/login/LoginActivity.kt
@@ -8,6 +8,7 @@ import com.tolstoy.zurichat.R
 import com.tolstoy.zurichat.ui.activities.HomePageActivity
 import com.tolstoy.zurichat.util.setUpApplicationTheme
 import com.tolstoy.zurichat.ui.activities.MainActivity
+import com.tolstoy.zurichat.ui.verifyemail.VerifyEmailActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,10 +16,10 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
         // This setups application theme to value stored in sharedPref
         setUpApplicationTheme(this)
-        val button: Button = findViewById(R.id.btn_verify_email1)
 
+        val button: Button = findViewById(R.id.btn_verify_email1)
         button.setOnClickListener {
-            val int = Intent(this, MainActivity::class.java)
+            val int = Intent(this, VerifyEmailActivity::class.java)
             startActivity(int)
         }
     }
--- app/src/main/java/com/tolstoy/zurichat/ui/verifyemail/VerifyEmailActivity.kt
+++ app/src/main/java/com/tolstoy/zurichat/ui/verifyemail/VerifyEmailActivity.kt
@@ -18,5 +18,10 @@ class VerifyEmailActivity : AppCompatActivity() {
         // This setups application theme to value stored in sharedPref
         setUpApplicationTheme(this)
 
+        val btn_verify_email: Button = findViewById(R.id.btn_verify_email)
+        btn_verify_email.setOnClickListener {
+            val int = Intent(this, OTP_Page::class.java)
+            startActivity(int)
+        }
     }
 }
