dd40c821bacacb061dff3ee9b983f4881c820fe0
geofferyj <geofferyjoseph1@gmail.com>
2021-09-03 17:33:35 +0100

    Reworked the login flow, Used fragments instead of different activities for each page, also added validation for the fields.

--- app/src/main/java/com/tolstoy/zurichat/ui/login/LoginActivity.kt
+++ app/src/main/java/com/tolstoy/zurichat/ui/login/LoginActivity.kt
@@ -8,6 +8,7 @@ import com.tolstoy.zurichat.R
 import com.tolstoy.zurichat.ui.activities.HomePageActivity
 import com.tolstoy.zurichat.util.setUpApplicationTheme
 import com.tolstoy.zurichat.ui.activities.MainActivity
+import com.tolstoy.zurichat.ui.otp.OTP_Page
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,11 +16,6 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
         // This setups application theme to value stored in sharedPref
         setUpApplicationTheme(this)
-        val button: Button = findViewById(R.id.btn_verify_email1)
 
-        button.setOnClickListener {
-            val int = Intent(this, MainActivity::class.java)
-            startActivity(int)
-        }
     }
 }
