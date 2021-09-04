46ced58e06effbd2de2e9a6f486745764d1d630e
electrosalaf <51977981+electrosalaf@users.noreply.github.com>
2021-09-04 15:07:47 +0100

    Implemented user profile picture

--- app/src/main/java/com/tolstoy/zurichat/ui/activities/ProfileActivity.kt
+++ app/src/main/java/com/tolstoy/zurichat/ui/activities/ProfileActivity.kt
@@ -1,15 +1,183 @@
 package com.tolstoy.zurichat.ui.activities
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.tolstoy.zurichat.R
+import kotlinx.android.synthetic.main.activity_profile.*
+import java.io.File
+import java.nio.ByteBuffer
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+typealias LumaListener = (luma: Double) -> Unit
+
 
 class ProfileActivity: AppCompatActivity() {
+
+    private var imageCapture: ImageCapture? = null
+
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
 
+        // Set up the listener for take photo icon
+        take_photo_icon.setOnClickListener { takePhoto() }
+
+        outputDirectory = getOutputDirectory()
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    private fun startCamera() {
+     val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+     cameraProviderFuture.addListener(Runnable {
+         // Used to bind the lifecycle of cameras to the lifecycle owner
+         val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+         // Preview
+         val preview = Preview.Builder()
+             .build()
+             .also {
+                 it.setSurfaceProvider(viewFinder.surfaceProvider)
+             }
+
+         imageCapture = ImageCapture.Builder()
+             .build()
+
+         val imageAnalyzer = ImageAnalysis.Builder()
+             .build()
+             .also {
+                 it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
+                     Log.d(TAG, "Average luminosity: $luma")
+                 })
+             }
+
+         // Select front camera as a default
+         val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+
+         try {
+             // Unbind use cases before binding
+             cameraProvider.unbindAll()
+
+             // Bind use cases to camera
+             cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalyzer)
+
+         } catch (exc: Exception) {
+             Log.e(TAG, "Use case binding failed", exc)
+         }
+
+     }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun takePhoto() {
+
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create time-stamped output file to hold the image
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat(FILENAME_FORMAT, Locale.US
+            ).format(System.currentTimeMillis()) + ".jpg")
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions, ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+                    val msg = "Photo capture succeeded: $savedUri"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+                }
+            })
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() } }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this, "Permission not granted.", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    private class LuminosityAnalyzer(private val listener: LumaListener) : ImageAnalysis.Analyzer {
+
+        private fun ByteBuffer.toByteArray(): ByteArray {
+            rewind()    // Rewind the buffer to zero
+            val data = ByteArray(remaining())
+            get(data)   // Copy the buffer into a byte array
+            return data // Return the byte array
+        }
+
+        override fun analyze(image: ImageProxy) {
+
+            val buffer = image.planes[0].buffer
+            val data = buffer.toByteArray()
+            val pixels = data.map { it.toInt() and 0xFF }
+            val luma = pixels.average()
+
+            listener(luma)
+
+            image.close()
+        }
+    }
+
+
+    companion object {
+        private const val TAG = "Camera"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     }
 }
