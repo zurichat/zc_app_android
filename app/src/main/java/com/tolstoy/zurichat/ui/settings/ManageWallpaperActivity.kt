604edb83df881111c0f777aafadddfdac3649d5e
Efobi-Francis <francisefobi@gmail.com>
2021-09-04 22:22:40 +0100

    feat: implemented wallpaper settings layout

---
+++ app/src/main/java/com/tolstoy/zurichat/ui/settings/ManageWallpaperActivity.kt
@@ -0,0 +1,13 @@
+package com.tolstoy.zurichat.ui.settings
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tolstoy.zurichat.R
+
+class ManageWallpaperActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wallpaper_settings)
+
+    }
+}
