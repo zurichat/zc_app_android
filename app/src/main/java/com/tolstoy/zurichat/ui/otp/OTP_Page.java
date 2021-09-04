62183e84f55970249148bb2cc3969aa48e5c50ba
Nwevo Peace Chinedu <nwevopeacechinedu@gmail.com>
2021-09-03 16:51:11 -0800

    Verify OTP screen

--- app/src/main/java/com/tolstoy/zurichat/ui/otp/OTP_Page.java
+++ app/src/main/java/com/tolstoy/zurichat/ui/otp/OTP_Page.java
@@ -1,16 +1,34 @@
 package com.tolstoy.zurichat.ui.otp;
 
+import static com.tolstoy.zurichat.util.UtilitiesKt.setUpApplicationTheme;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.tolstoy.zurichat.R;
 
 public class OTP_Page extends AppCompatActivity {
+    private Button otpVerifyCodeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp_page);
+
+        // This setups application theme to value stored in sharedPref
+        setUpApplicationTheme(this);
+
+        otpVerifyCodeBtn = findViewById(R.id.otpVerifyCodeBtn);
+        otpVerifyCodeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), VerifyOTP.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/tolstoy/zurichat/ui/otp/VerifyOTP.kt
@@ -0,0 +1,12 @@
+package com.tolstoy.zurichat.ui.otp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tolstoy.zurichat.R
+
+class VerifyOTP : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verify_otp)
+    }
+}
