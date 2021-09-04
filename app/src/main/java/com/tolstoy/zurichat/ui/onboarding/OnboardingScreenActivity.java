e2e902568a7768de2059e90ba47b45419dad184c
Abax <abassife@gmail.com>
2021-09-04 13:21:16 +0100

    Turn on/off screen notification

--- app/src/main/java/com/tolstoy/zurichat/ui/onboarding/OnboardingScreenActivity.java
+++ app/src/main/java/com/tolstoy/zurichat/ui/onboarding/OnboardingScreenActivity.java
@@ -3,35 +3,24 @@ package com.tolstoy.zurichat.ui.onboarding;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.HandlerThread;
-import android.os.Looper;
-import android.widget.Button;
-import android.widget.Toast;
+
 
 import com.tbuonomo.viewpagerdotsindicator.DotsIndicator;
 import com.tolstoy.zurichat.R;
 import com.tolstoy.zurichat.ui.adapters.OnboardingScreenAdapter;
-import com.tolstoy.zurichat.ui.settings.Notification;
 
 public class OnboardingScreenActivity extends AppCompatActivity {
-
-    private ViewPager viewPager;
-    private OnboardingScreenAdapter adapter;
-    private DotsIndicator dotsIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding_screen);
 
-        adapter = new OnboardingScreenAdapter(getSupportFragmentManager(), this);
-        viewPager = findViewById(R.id.pager);
+        OnboardingScreenAdapter adapter = new OnboardingScreenAdapter(getSupportFragmentManager(), this);
+        ViewPager viewPager = findViewById(R.id.pager);
         viewPager.setAdapter(adapter);
-        dotsIndicator =  findViewById(R.id.dots_indicator);
+        DotsIndicator dotsIndicator = findViewById(R.id.dots_indicator);
         dotsIndicator.setViewPager(viewPager);
     }
 
