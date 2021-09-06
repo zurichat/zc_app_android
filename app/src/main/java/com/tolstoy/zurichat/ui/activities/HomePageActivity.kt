82bfeebf243c2446157ca82c7997f3f12914385b
Ibukun Okunoye <ibkokunoye@gmail.com>
2021-09-06 02:40:38 +0100

    Removed changes

--- app/src/main/java/com/tolstoy/zurichat/ui/activities/HomePageActivity.kt
+++ app/src/main/java/com/tolstoy/zurichat/ui/activities/HomePageActivity.kt
@@ -38,36 +38,6 @@ class HomePageActivity : AppCompatActivity() {
 //        setSupportActionBar(mToolbar)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-
-        //
-        //Request permission for accessing media and files from storage
-        val requestPermissionLauncher =
-            registerForActivityResult(
-                ActivityResultContracts.RequestPermission())
-            { isGranted: Boolean ->
-                //Shows Toast message if permission is granted or denied.
-                if (isGranted) {
-                    Toast.makeText(this, "Permission Granted!", Toast.LENGTH_LONG).show()
-                } else {
-                    Toast.makeText(this, "Permission Denied!", Toast.LENGTH_LONG).show()
-                }
-            }
-
-        //This runs automatically,
-        //This checks if the permission has been granted, if it has, pass, else, it request for the permission using the function above
-        //Comment this if and else statements to prevent permission from showing on startup.
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.READ_EXTERNAL_STORAGE
-            ) == PackageManager.PERMISSION_GRANTED ) {
-            //Pass
-        }
-        else {//Request permission
-            requestPermissionLauncher.launch(
-                Manifest.permission.READ_EXTERNAL_STORAGE
-            )
-        }
-
         mViewPager2 = findViewById(R.id.pager)
         homeFragmentPagerAdapter = HomeFragmentPagerAdapter(this)
         mViewPager2.adapter = homeFragmentPagerAdapter
