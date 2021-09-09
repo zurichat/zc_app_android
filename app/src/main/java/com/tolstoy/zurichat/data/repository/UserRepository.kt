f0b5af23ef0dc329938328507ca0dbf669070a95
Peculiar Umeh <35475543+peculiaruc@users.noreply.github.com>
2021-09-09 12:58:49 +0100

    Database Setup

--- app/src/main/java/com/tolstoy/zurichat/data/repository/UserRepository.kt
+++ app/src/main/java/com/tolstoy/zurichat/data/repository/UserRepository.kt
@@ -1,4 +1,7 @@
 package com.tolstoy.zurichat.data.repository
 
-class UserRepository {
+import android.content.Context
+import com.tolstoy.zurichat.data.remoteSource.RetrofitService
+
+class UserRepository( private val retrofitService: RetrofitService, context: Context){
 }
