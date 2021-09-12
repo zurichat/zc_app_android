19378337f89288a55adb1c60bdd27eda9f5b9749
EricoMartin <martinirex@yahoo.co.uk>
2021-09-12 08:29:33 +0100

    Feat: Dm and Channel flow

--- app/src/main/java/com/tolstoy/zurichat/ui/fragments/channel_chat/ChannelChatFragment.kt
+++ app/src/main/java/com/tolstoy/zurichat/ui/fragments/channel_chat/ChannelChatFragment.kt
@@ -2,13 +2,24 @@ package com.tolstoy.zurichat.ui.fragments.channel_chat
 
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import android.widget.PopupWindow
+import androidx.appcompat.widget.Toolbar
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import com.tolstoy.zurichat.R
 import com.tolstoy.zurichat.databinding.FragmentChannelChatBinding
 import dev.ronnie.github.imagepicker.ImagePicker
+import java.util.*
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import timber.log.Timber
+
 
 class ChannelChatFragment : Fragment() {
     private lateinit var binding: FragmentChannelChatBinding
@@ -17,8 +28,23 @@ class ChannelChatFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        (requireActivity() as AppCompatActivity).apply {
+            setSupportActionBar(view?.findViewById(R.id.channel_chat_toolbar))
+//            setupActionBarWithNavController(findNavController())
+        }
         binding = FragmentChannelChatBinding.inflate(inflater, container, false)
+
+        val callback: OnBackPressedCallback =
+            object : OnBackPressedCallback(true /* enabled by default */) {
+                override fun handleOnBackPressed() {
+                    // Handle the back button event
+                    NavHostFragment.findNavController(ChannelChatFragment()).navigateUp()
+                    Timber.e("handleOnBackPressed")
+                }
+            }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
+
         return binding.root
     }
 
--- app/src/main/java/com/tolstoy/zurichat/ui/login/screens/LoginFragment.kt
+++ app/src/main/java/com/tolstoy/zurichat/ui/login/screens/LoginFragment.kt
@@ -1,34 +1,45 @@
 package com.tolstoy.zurichat.ui.login.screens
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.navigation.fragment.findNavController
 import com.tolstoy.zurichat.R
 import com.tolstoy.zurichat.databinding.FragmentLoginBinding
 import com.tolstoy.zurichat.databinding.FragmentSignupBinding
+import com.tolstoy.zurichat.ui.activities.DMActivity
+import com.tolstoy.zurichat.ui.activities.MainActivity
 
 class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val textView = binding.textViewRegister
+        val btn = binding.buttonSignUp
 
         textView.setOnClickListener(fun(it: View){
             findNavController().navigate(R.id.signupFragment)
         })
+        btn.setOnClickListener {
+            val intent = Intent(context, MainActivity::class.java)
+            context?.startActivity(intent)
+        }
     }
 
 }
