2bc00e9d2195f8e4d5bb05abd78e5a4ce43bbaaf
Ebenezer Justice <justiceebenezer4@gmail.com>
2021-09-13 18:34:34 +0100

    feat: Implemented user change phone number dialog

--- app/src/main/java/com/tolstoy/zurichat/ui/activities/ProfileActivity.kt
+++ app/src/main/java/com/tolstoy/zurichat/ui/activities/ProfileActivity.kt
@@ -2,11 +2,15 @@ package com.tolstoy.zurichat.ui.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
 import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.tolstoy.zurichat.R
+import timber.log.Timber
 
 class ProfileActivity: AppCompatActivity() {
 
@@ -16,6 +20,7 @@ class ProfileActivity: AppCompatActivity() {
 
         val about = findViewById<ImageView>(R.id.edit_about)
         val camera = findViewById<ImageView>(R.id.img_camera)
+        val phoneEdit = findViewById<ImageView>(R.id.imgPhone_editBT)
 
         about.setOnClickListener {
             startActivity(Intent(this, ProfileAboutActivity::class.java))
@@ -29,7 +34,32 @@ class ProfileActivity: AppCompatActivity() {
             dialog.show()
 
         }
+
+        // The following lines of code creates a dialog to change the Phone Number of the
+        // user.
+        phoneEdit.setOnClickListener{
+
+            val builder = AlertDialog.Builder(this)
+            val inflater = layoutInflater
+            val dialogLayout = inflater.inflate(R.layout.phoneno_edittext, null)
+            val editText = dialogLayout.findViewById<EditText>(R.id.et_phoneno)
+
+            with(builder){
+                setTitle("Edit Phone Number")
+                setPositiveButton("Save"){ _, _ ->
+                    editText.text.toString()
+                }
+                setNegativeButton("Cancel") { _, _ ->
+                    Timber.d("This button clicked sucessfully!!")
+                }
+                setView(dialogLayout)
+                show()
+            }
+
+
+        }
     }
+
 
 
 
