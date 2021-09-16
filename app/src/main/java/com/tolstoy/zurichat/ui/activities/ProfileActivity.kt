17aaef3f7f245e9b8a8ff8cfd4f760c00c70bc54
Abdulhamid Oyedele Oyempemi <oyempemia@gmail.com>
2021-09-16 20:50:45 +0100

    Update profile details service call

--- app/src/main/java/com/tolstoy/zurichat/ui/activities/ProfileActivity.kt
+++ app/src/main/java/com/tolstoy/zurichat/ui/activities/ProfileActivity.kt
@@ -2,6 +2,7 @@ package com.tolstoy.zurichat.ui.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -11,14 +12,21 @@ import com.tolstoy.zurichat.ui.profile.data.ProfilePayload
 import com.tolstoy.zurichat.ui.profile.data.ProfileResponse
 import com.tolstoy.zurichat.ui.profile.network.Constants
 import com.tolstoy.zurichat.ui.profile.network.ProfileService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.Header
 import retrofit2.http.PATCH
 import retrofit2.http.Path
+import timber.log.Timber
 
 class ProfileActivity: AppCompatActivity() {
+
+    //token id
+    private var token: String? = null
 
     //prepare retrofit service
     private val retrofit: Retrofit = Retrofit.Builder()
@@ -49,6 +57,51 @@ class ProfileActivity: AppCompatActivity() {
         }
     }
 
+    //update profile details
+    private fun updateProfile() {
 
+        //demo data
+        val profileData = ProfilePayload("Zuri chat member",
+            "PorayMan",
+            "09876543212")
+
+        val authToken: String? = token
+
+        val call: Call<ProfileResponse> = retrofitService.updateProfile(authToken, Constants.ORG_ID, Constants.MEM_ID, profileData)
+
+        call.enqueue(object : Callback<ProfileResponse> {
+            override fun onResponse(
+                call: Call<ProfileResponse>,
+                response: Response<ProfileResponse>?
+            ) {
+
+                if(response!!.isSuccessful) {
+                    Log.i("Login Response Result", response.body()!!.message)
+
+                } else {
+                    when(response.code()){
+                        400 -> {
+                            Log.e("Error 400", "invalid authorization")
+                        }
+                        404 -> {
+                            Log.e("Error 404", "Not Found")
+                        }
+                        401 -> {
+                            Log.e("Error 401", "No authorization or session expired")
+                        }
+                        else -> {
+                            Log.e("Error", "Generic Error")
+                        }
+                    }
+                }
+
+            }
+
+            override fun onFailure(call: Call<ProfileResponse>, t: Throwable) {
+                Timber.e(t.message.toString())
+            }
+
+        })
+    }
 
 }
