57abdd5a7b2356a002ae6ecfad651da9ce9c9b87
Peculiar Umeh <35475543+peculiaruc@users.noreply.github.com>
2021-09-16 14:56:00 +0100

    Fixes on login Screen

--- app/src/main/java/com/tolstoy/zurichat/ui/login/screens/RegisterUserFragment.java
+++ app/src/main/java/com/tolstoy/zurichat/ui/login/screens/RegisterUserFragment.java
@@ -81,7 +81,7 @@ public class RegisterUserFragment extends Fragment {
                     email.setError(null);
                     password.setError("Password cannot be empty");
                 }else if(!isValidPassword(userPassword.trim())){
-                    password.setError("Use special characters + alphanumeric keys");
+                    password.setError("Invalid password pattern");
                 }else if(!password.getEditText().getText().toString().equals(password2.getEditText().getText().toString())){
                     password2.setError("Password does not match");
                 }else{
