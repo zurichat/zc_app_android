d548a548ed58471d4e3147aadbe3e857343dd7da
Chukwuma Miracle Ebube <61353651+EbubeCode@users.noreply.github.com>
2021-09-17 13:34:34 +0100

    Fix: NewChannelActivity.kt
    Changed setContentView(binding.root) to setContentView(R.layout.activity_new_channel), in order to fix error due to lateinit.

--- app/src/main/java/com/tolstoy/zurichat/ui/newchannel/NewChannelActivity.kt
+++ app/src/main/java/com/tolstoy/zurichat/ui/newchannel/NewChannelActivity.kt
@@ -10,6 +10,7 @@ class NewChannelActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_new_channel)
+    
     }
 }
