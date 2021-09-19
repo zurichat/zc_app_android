109966b6aebfb4c2c09df8e825fc277a0a5195df
dapoanjorin <femidpo3@gmail.com>
2021-09-19 04:56:25 +0100

    implemented enter is send in chat settings

--- app/src/main/java/com/tolstoy/zurichat/ui/dm/DMFragment.kt
+++ app/src/main/java/com/tolstoy/zurichat/ui/dm/DMFragment.kt
@@ -1,17 +1,23 @@
 package com.tolstoy.zurichat.ui.dm
 
+import android.content.Context.MODE_PRIVATE
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.text.Editable
+import android.text.InputType
 import android.text.TextWatcher
 import android.view.Gravity
 import android.view.View
 import android.view.WindowManager
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
 import android.widget.PopupWindow
 import android.widget.Toast
 import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.preference.Preference
+import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.tolstoy.zurichat.R
 import com.tolstoy.zurichat.databinding.FragmentDmBinding
@@ -29,17 +35,26 @@ class DMFragment : Fragment(R.layout.fragment_dm) {
     private lateinit var binding: FragmentDmBinding
     private val attachmentPopup by lazy { PopupWindow(requireContext()) }
 
+    private var isEnterSend: Boolean = false
+
     private val sendDrawable by lazy {
         ResourcesCompat.getDrawable(requireContext().resources, R.drawable.ic_send, null)
     }
     private val speakDrawable by lazy {
-        ResourcesCompat.getDrawable(requireContext().resources, android.R.drawable.ic_btn_speak_now, null)
+        ResourcesCompat.getDrawable(
+            requireContext().resources,
+            android.R.drawable.ic_btn_speak_now,
+            null
+        )
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         imagePicker = ImagePicker(this)
+
+        isEnterSend = PreferenceManager.getDefaultSharedPreferences(requireContext())
+            .getBoolean("enter_to_send", false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -55,14 +70,29 @@ class DMFragment : Fragment(R.layout.fragment_dm) {
 
     private fun setupUI() {
 
+        // set keyboard to send if "enter is send" is set to true in settings
+        binding.messageinputDm.textinputMIMessage.apply {
+            if(isEnterSend) {
+                this.inputType = InputType.TYPE_CLASS_TEXT
+                this.imeOptions = EditorInfo.IME_ACTION_SEND
+            }
+        }
+
+        binding.messageinputDm.textinputMIMessage.setOnEditorActionListener { v, actionId, event ->
+            if(actionId == EditorInfo.IME_ACTION_SEND) {
+                sendMessage(binding.messageinputDm.textinputMIMessage.editableText)
+                true
+            } else {
+                false
+            }
+        }
+
+
         // set up the message input view
         binding.messageinputDm.also {
-            it.fabMIRecordAudio.setOnClickListener { _->
+            it.fabMIRecordAudio.setOnClickListener { _ ->
                 it.textinputMIMessage.text.also { editable ->
-                    if(editable.isNotBlank()) {
-                        adapter.addMessage(Message(0, editable.toString()))
-                        editable.clear()
-                    }
+                    sendMessage(editable)
                 }
             }
             it.imageMIAttachImage.setOnClickListener { takePictureCamera() }
@@ -70,8 +100,10 @@ class DMFragment : Fragment(R.layout.fragment_dm) {
                 val anchor = binding.messageinputDm.root
 //                val location = IntArray(2).apply { anchor.getLocationOnScreen(this) }
 //                val size = Size(attachmentPopup.contentView.measuredWidth, attachmentPopup.contentView.measuredHeight)
-                attachmentPopup.showAtLocation(anchor,Gravity.BOTTOM or Gravity.START,
-                    0, anchor.height)
+                attachmentPopup.showAtLocation(
+                    anchor, Gravity.BOTTOM or Gravity.START,
+                    0, anchor.height
+                )
             }
             it.textinputMIMessage.addTextChangedListener(object : TextWatcher {
                 override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
@@ -80,7 +112,7 @@ class DMFragment : Fragment(R.layout.fragment_dm) {
                 override fun afterTextChanged(p0: Editable?) {
                     // checks if the the user types in a something
                     // if they do change the fab drawable
-                    if(p0?.isNotBlank() == true)
+                    if (p0?.isNotBlank() == true)
                         it.fabMIRecordAudio.setImageDrawable(sendDrawable)
                     else it.fabMIRecordAudio.setImageDrawable(speakDrawable)
                 }
@@ -106,8 +138,19 @@ class DMFragment : Fragment(R.layout.fragment_dm) {
         }
     }
 
-    private fun navigateToAttachmentScreen(media: MEDIA = MEDIA.IMAGE){
-        findNavController().navigate(DMFragmentDirections.actionDmFragmentToAttachmentsFragment(media))
+    private fun sendMessage(editable: Editable) {
+        if (editable.isNotBlank()) {
+            adapter.addMessage(Message(0, editable.toString()))
+            editable.clear()
+        }
+    }
+
+    private fun navigateToAttachmentScreen(media: MEDIA = MEDIA.IMAGE) {
+        findNavController().navigate(
+            DMFragmentDirections.actionDmFragmentToAttachmentsFragment(
+                media
+            )
+        )
     }
 
     /**function to take image using camera
