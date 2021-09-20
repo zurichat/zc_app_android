fbd88bd3b92665f9369c2114cae84d4922b0d410
Abdulhamid Oyedele Oyempemi <oyempemia@gmail.com>
2021-09-20 22:26:35 +0100

    Preparing Org response service

--- app/src/main/java/com/tolstoy/zurichat/ui/profile/network/ProfileService.kt
+++ app/src/main/java/com/tolstoy/zurichat/ui/profile/network/ProfileService.kt
@@ -2,6 +2,7 @@ package com.tolstoy.zurichat.ui.profile.network
 
 import com.tolstoy.zurichat.ui.profile.data.ProfilePayload
 import com.tolstoy.zurichat.ui.profile.data.ProfileResponse
+import com.tolstoy.zurichat.ui.profile.data.UserOrganizationResponse
 import retrofit2.Call
 import retrofit2.http.*
 
@@ -23,4 +24,10 @@ interface ProfileService {
         @Path("mem_id") mem_id: String,
         @Body profilePayload: ProfilePayload
     ): Call<ProfileResponse>
+
+    @Headers("Content-Type: application/json;charset=UTF-8")
+    @GET("users/{email}/organizations")
+    fun getUserOrg(
+        @Path("email") email: String
+    ): Call<UserOrganizationResponse>
 }
