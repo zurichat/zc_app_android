47e4e78d15196bb4bd1f672810c2f903b1faa937
etinosa123 <lawsonosazuwa@gmail.com>
2021-09-20 07:43:12 +0100

    git commit -m "feat:implemented document screen"

---
+++ app/src/main/java/com/tolstoy/zurichat/ui/activities/DocumentsSelectedActivity.java
@@ -0,0 +1,16 @@
+package com.tolstoy.zurichat.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tolstoy.zurichat.R;
+
+public class DocumentsSelectedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_documents_selected);
+    }
+}
