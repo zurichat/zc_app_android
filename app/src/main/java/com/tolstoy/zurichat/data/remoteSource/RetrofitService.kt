cf8f6d1ab27d6e025a5e7a4547e4f11a1113e571
Peculiar Umeh <peculiarumeh02@gmail.com>
2021-09-24 19:32:59 +0100

    Forgot Password

--- app/src/main/java/com/tolstoy/zurichat/data/remoteSource/RetrofitService.kt
+++ app/src/main/java/com/tolstoy/zurichat/data/remoteSource/RetrofitService.kt
@@ -1,6 +1,7 @@
 package com.tolstoy.zurichat.data.remoteSource
 
 import com.tolstoy.zurichat.models.*
+import kotlinx.coroutines.flow.Flow
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -14,6 +15,9 @@ interface RetrofitService {
 
     @POST("auth/login")
     suspend fun login(@Body loginBody: LoginBody): LoginResponse
+
+    @POST("account/request-password-reset-code")
+    suspend fun passwordreset(@Body passwordReset: PasswordReset): PassswordRestReponse
 
     @POST("users")
     fun register(@Body registerUser: RegisterUser?): Call<RegisterUser?>?
