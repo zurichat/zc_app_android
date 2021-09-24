00f3638fc56827a44493a98a63bf90535e00821a
tolulonge <tolulopelonge945@gmail.com>
2021-09-24 10:55:40 +0100

    created model classes and interface definition for the messages retrieve endpoint

--- app/src/main/java/com/tolstoy/zurichat/ui/fragments/networking/JoinNewChannel.kt
+++ app/src/main/java/com/tolstoy/zurichat/ui/fragments/networking/JoinNewChannel.kt
@@ -2,6 +2,7 @@ package com.tolstoy.zurichat.ui.fragments.networking
 
 import com.tolstoy.zurichat.ui.fragments.model.JoinChannelUser
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
 
@@ -10,4 +11,8 @@ interface JoinNewChannel {
 
     @POST("v1/{org_id}/channels/{channel_id}/members/")
     suspend fun joinChannel(@Path("org_id") organizationId : String, @Path("channel_id") channelId : String,@Body user : JoinChannelUser): JoinChannelUser
+
+    @GET("/v1/{org_id}/channels/{channel_id}/messages/")
+    suspend fun retrieveAllMessages(@Path("org_id") organizationId : String, @Path("channel_id") channelId : String): JoinChannelUser
+
 }
