e484ef7f010a511bd4f9e2f7924fe47a51ee183c
tolulonge <tolulopelonge945@gmail.com>
2021-09-24 11:44:56 +0100

    completed network call to retrieve all messages from a particular channel

--- app/src/main/java/com/tolstoy/zurichat/ui/fragments/networking/JoinNewChannel.kt
+++ app/src/main/java/com/tolstoy/zurichat/ui/fragments/networking/JoinNewChannel.kt
@@ -1,5 +1,6 @@
 package com.tolstoy.zurichat.ui.fragments.networking
 
+import com.tolstoy.zurichat.ui.fragments.model.AllChannelMessages
 import com.tolstoy.zurichat.ui.fragments.model.JoinChannelUser
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -12,7 +13,8 @@ interface JoinNewChannel {
     @POST("v1/{org_id}/channels/{channel_id}/members/")
     suspend fun joinChannel(@Path("org_id") organizationId : String, @Path("channel_id") channelId : String,@Body user : JoinChannelUser): JoinChannelUser
 
-    @GET("/v1/{org_id}/channels/{channel_id}/messages/")
-    suspend fun retrieveAllMessages(@Path("org_id") organizationId : String, @Path("channel_id") channelId : String): JoinChannelUser
+    // Endpoint to retrieve all messages in a channel
+    @GET("v1/{org_id}/channels/{channel_id}/messages/")
+    suspend fun retrieveAllMessages(@Path("org_id") organizationId : String, @Path("channel_id") channelId : String): AllChannelMessages
 
 }
