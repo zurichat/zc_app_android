48687f166ec7003b2caef330afd383496049e01c
Ebenezer Justice <justiceebenezer4@gmail.com>
2021-09-30 10:03:05 +0100

    feat: implemented the camera acess feature

--- app/src/main/java/com/tolstoy/zurichat/ui/activities/OpenCameraActivity.kt
+++ app/src/main/java/com/tolstoy/zurichat/ui/activities/OpenCameraActivity.kt
@@ -2,12 +2,10 @@ package com.tolstoy.zurichat.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.tolstoy.zurichat.R
 
-class DocumentSentActivity : AppCompatActivity() {
+class OpenCameraActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_document_sent)
-
+        setContentView(R.layout.activity_open_camera)
     }
 }
