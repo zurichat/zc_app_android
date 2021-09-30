458a723815d74502cc6d30188c88b87cbf86fe38
kolawole <akinde.kolawole@gmail.com>
2021-09-30 06:00:55 +0100

    implementation of organization members

--- app/src/main/java/com/tolstoy/zurichat/data/localSource/AppDatabase.kt
+++ app/src/main/java/com/tolstoy/zurichat/data/localSource/AppDatabase.kt
@@ -2,11 +2,13 @@ package com.tolstoy.zurichat.data.localSource
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.tolstoy.zurichat.data.localSource.dao.OrganizationMembersDao
 import com.tolstoy.zurichat.data.localSource.dao.UserDao
 import com.tolstoy.zurichat.models.User
 
-@Database(entities = [User::class], version = 1, exportSchema = false)
+@Database(entities = [User::class, OrganizationMemberEntity::class], version = 2, exportSchema = false)
  abstract class AppDatabase: RoomDatabase() {
 
      abstract fun userDao(): UserDao
+     abstract fun organizationMembersDao(): OrganizationMembersDao
 }
