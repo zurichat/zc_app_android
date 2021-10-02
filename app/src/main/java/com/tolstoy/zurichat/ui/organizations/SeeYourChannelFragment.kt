8a0c36e65847109b6ac1ee3d6260426014a1224e
Auwalu Hamza <45801980+afhamu@users.noreply.github.com>
2021-10-02 07:35:04 +0100

    feat: implemented Show the organization that the user is currently logged in after creating new organization

--- app/src/main/java/com/tolstoy/zurichat/ui/organizations/SeeYourChannelFragment.kt
+++ app/src/main/java/com/tolstoy/zurichat/ui/organizations/SeeYourChannelFragment.kt
@@ -2,6 +2,7 @@ package com.tolstoy.zurichat.ui.organizations
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.tolstoy.zurichat.R
@@ -17,7 +18,8 @@ class SeeYourChannelFragment : Fragment(R.layout.fragment_see_your_channel) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.btnSeeYourChannel.setOnClickListener {
-            Navigation.findNavController(it).navigate(R.id.action_seeYourChannelFragment_to_homeScreenFragment)
+            val bundle = bundleOf("org_name" to arguments?.getString("org_name"))
+            Navigation.findNavController(it).navigate(R.id.action_seeYourChannelFragment_to_homeScreenFragment, bundle)
         }
     }
 
