145f500aa4f007e92d2f18f1f569ce3acf619b75
CephasPeter <52112132+CephasPeter@users.noreply.github.com>
2021-10-12 03:15:44 +0100

    Update NextFragment.kt

--- app/src/main/java/com/zurichat/app/ui/organizations/NextFragment.kt
+++ app/src/main/java/com/zurichat/app/ui/organizations/NextFragment.kt
@@ -38,7 +38,9 @@ class NextFragment : Fragment(R.layout.fragment_add_to_organization) {
         binding.toolbarAddTo.setNavigationOnClickListener { requireActivity().onBackPressed() }
         binding.apply {
             addByEmailButton.setOnClickListener(fun(_: View){
-               val action  = NextFragmentDirections.actionNextFragmentToAddByEmailFragment(organizationName, organizationId)
+                val action  = NextFragmentDirections.actionNextFragmentToAddByEmailFragment()
+                action.organizationName = organizationName
+                action.organizationId = organizationId
                 findNavController().navigate(action)
             })
             addFromContactsButton
