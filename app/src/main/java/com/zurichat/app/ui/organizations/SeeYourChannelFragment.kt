5c9db245d8cb99e1868975fa430dc423782f7ad2
tolulonge <tolulopelonge945@gmail.com>
2021-10-12 10:56:48 +0100

    fixed crash issue when creating an organization while signed in

--- app/src/main/java/com/zurichat/app/ui/organizations/SeeYourChannelFragment.kt
+++ app/src/main/java/com/zurichat/app/ui/organizations/SeeYourChannelFragment.kt
@@ -20,7 +20,7 @@ class SeeYourChannelFragment : Fragment(R.layout.fragment_see_your_channel) {
 // navigate from see your channel fragment to mainActivity passing in the organization
         binding.btnSeeYourChannel.setOnClickListener {
             val bundle = bundleOf("org_name" to arguments?.getString("org_name"))
-            Navigation.findNavController(it).navigate(R.id.action_seeYourChannelFragment_to_mainActivity, bundle)
+            Navigation.findNavController(it).navigate(R.id.action_seeYourChannelFragment_to_homeScreenFragment, bundle)
         }
     }
 
